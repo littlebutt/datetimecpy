a7911d7af63b7cfcc08ad555ea579cd8b4cde850
littlebutt <luogan199686@gmail.com>
2023-04-21 20:58:27 +0800

    feat: design the date object

---
+++ include/date.h
@@ -0,0 +1,35 @@
+#ifndef DATE_H
+#define DATE_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define PY_SSIZE_T_CLEAN
+#include <Python.h>
+#include "structmember.h"
+
+static PyTypeObject Date_type;
+
+typedef struct {
+    PyObject_HEAD
+
+    char* timestamp;
+
+} Date;
+
+static void Date_dealloc(PyObject* self);                                       // destruction method
+
+static PyObject* Date_new(PyTypeObject* type, PyObject* args, PyObject* kwds);  // __new__ method
+
+int Date_init(PyObject* self, PyObject* args, PyObject* kwds);                  // __init__ method
+
+static PyObject* Date_repr(PyObject* self);                                     // __repr__ method
+
+PyObject* Date_richcompare(PyObject* self, PyObject* other, int op);            // __lt__, __le__, __eq__, __ne__, __gt__, __ge__ methods
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // DATE_H
